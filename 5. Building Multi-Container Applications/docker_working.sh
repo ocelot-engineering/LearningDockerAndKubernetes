cfae0d96b0ab2584f2d69a5dfdec61da6d9032d3
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2022-11-29 19:39:56 +0000

    Lesson 5: finish multi-container

--- 5. Building Multi-Container Applications/docker_working.sh
+++ 5. Building Multi-Container Applications/docker_working.sh
@@ -3,7 +3,10 @@ docker network create goals-net
 docker network ls # confirm creation
 
 # 2. Dockerise Mongo DB and ensure data persists
-docker run --name mongodb --network goals-net --rm -d mongo
+# add user and (bad) password require credentials to login
+# docs for connection: https://www.mongodb.com/docs/manual/reference/connection-string/
+# docs for image and named volume: https://hub.docker.com/_/mongo
+docker run --name mongodb --network goals-net --rm -d -v data:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=open_secret mongo
 docker ps # check container is running
 
 # 3. Dockerise backend and ensure logs exists and source code is updated live (i.e. changes in source don't require image rebuild)
@@ -12,13 +15,14 @@ docker ps # check container is running
 # update app.js to no longer hardcode port and use environment variable from docker "process.env.PORT" instead
 # add named volume to store logs "-v feedback:/app/feedback"
 # set bind mount -v "$(pwd):/app:ro" to read app code
+# set anonymous volume for node_modules by /app/node_modules, to prevent the live code overwrite
 # create anonymous volume to overwrite node_modules directory
 # chmod -R 775 backend # grant permission to live update code
 cd backend
 # update localhost to host.docker.internal for localhost, but given it's running in network update with container name 
 docker build -t goals-backend . # build image
 docker image ps
-docker run --rm -d --name goals-backend --network goals-net -p 80:80 -v logs:/app/logs -v "$(pwd):/app:ro" -v /app/node_modules goals-backend:latest
+docker run --rm -d --name goals-backend --network goals-net -e MONGODB_PASSWORD=open_secret -p 80:80 -v logs:/app/logs -v "$(pwd):/app:ro" -v /app/node_modules goals-backend:latest
 
 # 4. Dockerise frontend with live source code update
 # Publish on port 3000 to see the fontend
@@ -29,4 +33,4 @@ docker run --rm -d --name goals-backend --network goals-net -p 80:80 -v logs:/ap
 #  side rather than server side. Therefore we should pubish the ports of the backend so the frontend can interact with it
 cd ../frontend
 docker build -t goals-frontend .
-docker run -it --rm -d --name goals-frontend --network goals-net -p 3000:3000 goals-frontend:latest
+docker run -it --rm -d --name goals-frontend -p 3000:3000 -v "$(pwd)/src:/app/src:ro" goals-frontend:latest
