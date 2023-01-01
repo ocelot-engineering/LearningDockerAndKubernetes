b96c19c26b340ca7febe48fd132940e05eda0d91
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-01-01 19:11:00 +0000

    Lesson 12: kubernetes in action - add mini app - add kubectl and minikube commands

---
+++ 12. Kubernetes in Action/kub-action-01-starting-setup/kubernetes-commands.sh
@@ -0,0 +1,28 @@
+# Run in powershell
+
+# Imperative approach
+minikube start # start cluster
+minikube status # confirm cluster is up and running
+
+kubectl create deployment first-app --image=<remote image name>
+kubectl delete deployment first-app
+kubectl get deployments # get deployments info
+kubectl get pods # get pod info
+
+minikube dashboard # see daashboard in browser
+
+kubectl expose deployment first-app --type=LoadBalancer --port=8080
+kubectl get services
+minikube service first-app # returns a url for a service in the local cluster (bascially maps a port to an IP)
+
+kubectl scale deployment/first-app --replicas=3 # creates 3 pods, load balancer handles routing to pods
+
+kubectl set image deployment/first-app kub-first-app=<remote image name> # update image
+kubectl rollout status deployment/first-app # see status of image update and reployment
+kubectl rollout undo deployment/first-app # undo latest deployment
+kubectl rollout undo deployment/first-app --to-revision=1 # rollback to a specific revision
+
+kubectl delete service first-app
+kubectl delete deployment first-app
+
+# Declarative approach
---
+++ kubernetes-commands.sh
@@ -0,0 +1,10 @@
+# minikube
+minikube start # start cluster
+minikube start --driver=docker # default
+minikube start --driver=virtualbox
+
+minikube dashboard
+minikube status
+
+kubectl get po -A
+kubectl help
